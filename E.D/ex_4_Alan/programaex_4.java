66ac623962bbc7968b65cea8a479dfe675d32ba7
Alan <alanrabinerson@gmail.com>
2024-10-29 00:33:57 +0100

    añadidas las actividades 4 y 5

---
+++ E.D/ex_4_Alan/programaex_4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+Scanner sc = new Scanner(System.in);
+System.out.println("Escribe el primer numero");
+int numero1 = sc.nextInt();
+System.out.println("Escribe el segundo numero");
+int numero2 = sc.nextInt();
+
+int producte= numero1 + numero2;
+System.out.println(numero1 + " + " + numero2 + " = " + producte);
+
+int producte= numero1 * numero2;
+System.out.println(numero1 + " * " + numero2 + " = " + producte);
+
+
---
+++ E.D/ex_5_Alan/ex_5.java
@@ -0,0 +1,3 @@
+public class ex_5 {
+    System.out.println("Hello world!")
+}
