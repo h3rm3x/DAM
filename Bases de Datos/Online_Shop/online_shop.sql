c27af5ab48946e982cd898f10716215e89bd690a
h3rm3x <alanrabinerson@gmail.com>
2024-12-12 13:29:43 +0100

    cambiada la vista de la tabla de productos y mejor la estructura de la tabla de order

--- Bases de Datos/Online_Shop/online_shop.sql
+++ Bases de Datos/Online_Shop/online_shop.sql
@@ -59,7 +59,7 @@ CREATE TABLE `adress_customer`(
     `adress_id` int(11) NOT NULL,
     `customer_id` int(11) NOT NULL,
     FOREIGN KEY (`adress_id`) REFERENCES `adress`(`adress_id`),
-    FOREIGN KEY (`customer_id`) REFERENCES `customer`(`customer_id`)
+    FOREIGN KEY (`customer_id`) REFERENCES `customer`(`customer_id`),
     PRIMARY KEY(adress_id, customer_id)
 ) ;
 
@@ -74,7 +74,7 @@ CREATE TABLE `payment_customer`(
 CREATE TABLE `category`(
     `category_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` varchar(100) NOT NULL,
-    `description` varchar(100) NOT NULL,
+    `description` varchar(100) NOT NULL
 ) ;
 
 CREATE TABLE `product_category`(
@@ -83,10 +83,10 @@ CREATE TABLE `product_category`(
     PRIMARY KEY(product_id, category_id),
     FOREIGN KEY (`product_id`) REFERENCES `product`(`product_id`),
     FOREIGN KEY (`category_id`) REFERENCES `category`(`category_id`)
-;)
+);
 
 CREATE TABLE `order`(
-    `order_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `order_number` int(11) NOT NULL,
     `customer_id` int(11) NOT NULL,
     `product_id` int(11) NOT NULL,
     `quantity` int(11) NOT NULL,
@@ -96,7 +96,8 @@ CREATE TABLE `order`(
     FOREIGN KEY (`product_id`) REFERENCES `product`(`product_id`),
     FOREIGN KEY (`adress_id`) REFERENCES `adress`(`adress_id`),
     FOREIGN KEY (`method_id`) REFERENCES `payment_method`(`method_id`),
-    FOREIGN KEY (`customer_id`) REFERENCES `customer`(`customer_id`)
+    FOREIGN KEY (`customer_id`) REFERENCES `customer`(`customer_id`),
+    PRIMARY KEY(order_number, product_id, customer_id)
 ) ;
 
 
@@ -194,8 +195,8 @@ INSERT INTO `payment_method` (`method_name`,`account_number`,`card_number`, `exp
 ('Mastercard Debit Card', null, '987654321098', '12/25', '321'),
 ('Bank transfer', '234567890', null, null, null),
 ('Mastercard Credit Card', null, '987654321907', '12/25', '321'),
-('Bank transfer', '212367890', null, null, null)
-('Visa Debit Card', null, '134567901234', '12/25', '123'),
+('Bank transfer', '212367890', null, null, null),
+('Visa Debit Card', null, '134567901234', '12/25', '123');
 
 INSERT INTO `payment_customer` (`method_id`, `customer_id`) VALUES
 (1, 1),
@@ -240,29 +241,24 @@ INSERT INTO `shopping_cart` (`customer_id`, `product_id`, `quantity`) VALUES
 (8, 7, 9),
 (10, 8, 10);	
 
-INSERT INTO `order` (`customer_id`, `product_id`, `quantity`, `order_date`, `adress_id`, `method_id`) VALUES
-(1, 1, 1, '2024-01-01 00:00:00', 1, 1),
-(2, 2, 2, '2024-01-02 00:00:00', 2, 2),
-(3, 3, 3, '2024-01-03 00:00:00', 3, 3),
-(4, 4, 4, '2024-01-04 00:00:00', 4, 4),
-(5, 5, 5, '2024-01-05 00:00:00', 5, 5),
-(6, 6, 6, '2024-01-06 00:00:00', 6, 6),
-(7, 7, 7, '2024-01-07 00:00:00', 7, 7),
-(8, 8, 8, '2024-01-08 00:00:00', 8, 8),
-(9, 9, 9, '2024-01-09 00:00:00', 9, 9),
-(10, 10, 10, '2024-01-10 00:00:00', 10, 10),
-(1, 9, 1, '2024-01-01 00:00:00', 1, 1),
-(3, 10, 2, '2024-01-12 00:00:00', 3, 3),
-(5, 1, 3, '2024-01-13 00:00:00', 5, 5),
-(7, 2, 4, '2024-01-14 00:00:00', 7, 7),
-(9, 3, 5, '2024-01-09 00:00:00', 9, 9),
-(2, 4, 6, '2024-01-16 00:00:00', 2, 2),
-(4, 5, 7, '2024-01-17 00:00:00', 4, 4),
-(6, 6, 8, '2024-01-06 00:00:00', 6, 6),
-(8, 7, 9, '2024-01-08 00:00:00', 8, 8),
-(10, 8, 10, '2024-01-10 00:00:00', 10, 10);
-
-
-
-
-
+INSERT INTO `order` (`order_number`,`customer_id`, `product_id`, `quantity`, `order_date`, `adress_id`, `method_id`) VALUES
+(1,1, 1, 1, '2024-01-01 00:00:00', 1, 1),
+(2,2, 2, 2, '2024-01-02 00:00:00', 2, 2),
+(3,3, 3, 3, '2024-01-03 00:00:00', 3, 3),
+(4, 4, 4, 4, '2024-01-04 00:00:00', 4, 4),
+(5, 5, 5, 5, '2024-01-05 00:00:00', 5, 5),
+(6, 6, 6, 6, '2024-01-06 00:00:00', 6, 6),
+(7, 7, 7, 7, '2024-01-07 00:00:00', 7, 7),
+(8, 8, 8, 8, '2024-01-08 00:00:00', 8, 8),
+(9, 9, 9, 9, '2024-01-09 00:00:00', 9, 9),
+(10, 10, 10, 10, '2024-01-10 00:00:00', 10, 10),
+(1, 1, 9, 1, '2024-01-01 00:00:00', 1, 1),
+(3, 3, 10, 2, '2024-01-03 00:00:00', 3, 3),
+(5, 5, 1, 3, '2024-01-05 00:00:00', 5, 5),
+(7, 7, 2, 4, '2024-01-07 00:00:00', 7, 7),
+(11, 9, 3, 5, '2024-01-11 00:00:00', 9, 9),
+(2, 2, 4, 6, '2024-01-02 00:00:00', 2, 2),
+(4, 4, 5, 7, '2024-01-04 00:00:00', 4, 4),
+(6, 4, 6, 8, '2024-01-06 00:00:00', 6, 6),
+(8, 8, 7, 9, '2024-01-08 00:00:00', 8, 8),
+(10, 10, 8, 10, '2024-01-10 00:00:00', 10, 10);
