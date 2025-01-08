2a82437d9d08f35a0ca0a954fd55022cb27c5e13
h3rm3x <alanrabinerson@gmail.com>
2025-01-08 22:47:06 +0100

    querys de la base de datos de la tienda online usando group by

--- Bases de Datos/Online_Shop/consultas_online_shop.sql
+++ Bases de Datos/Online_Shop/consultas_online_shop.sql
@@ -1,2 +1,23 @@
 SELECT *
 FROM product_view;
+--  total price per customer of those who have >100e in the shopping cart
+SELECT customer_id, first_name, last_name, SUM(total_price) AS total_price
+FROM shopping_cart_view
+GROUP BY customer_id, first_name, last_name
+HAVING total_price >= 100;
+-- total number of items per customer.
+SELECT customer_id, first_name, last_name, SUM(quantity) AS total_qty
+FROM shopping_cart_view
+GROUP BY customer_id, first_name, last_name;
+-- Average number of items put in the shopping cart
+SELECT customer_id, first_name, last_name, ROUND(avg(quantity), 1) AS average_items
+FROM shopping_cart_view
+GROUP BY customer_id, first_name, last_name;
+-- total sales per year
+SELECT  COUNT(*), YEAR(order_date) as YEAR
+FROM order_view
+GROUP BY YEAR(order_date);
+-- total sales per quarter
+SELECT  COUNT(*), QUARTER(order_date) as QUARTER
+FROM order_view
+GROUP BY QUARTER(order_date);
