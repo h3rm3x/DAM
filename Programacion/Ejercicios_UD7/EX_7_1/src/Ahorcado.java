ba8af8f8203f382ef32d269d6f860b037b545544
h3rm3x <alanrabinerson@gmail.com>
2025-02-06 23:33:43 +0100

    programa ahorcado finalizado

--- Programacion/Ejercicios_UD7/EX_7_1/src/Ahorcado.java
+++ Programacion/Ejercicios_UD7/EX_7_1/src/Ahorcado.java
@@ -1,16 +1,143 @@
 public class Ahorcado {
-    private String palabra;
-    private String palabraOculta;
-    private char[] letrasIntentadas;
-    private boolean juegoTerminado;
+    protected String palabra;
+    protected String palabraOculta;
+    protected static   char[] letrasIntentadas;
+    protected static boolean juegoTerminado;
+    public static String dibujo;
+    protected static int intentos;
+    protected static int intentosIncorrectos=0;
 
     public Ahorcado(String palabra) {
         this.palabra = palabra;
-        this.palabraOculta = palabra.replaceAll(" ", "_");
-        this.letrasIntentadas = new char[26];
+        this.palabraOculta = "-".repeat(palabra.length());
+        letrasIntentadas = new char[26];
         this.juegoTerminado = false;
 
     }
+
+    public String getPalabraOculta() {
+        return palabraOculta;
+    }
+
+    public boolean isJuegoTerminado() {
+        return juegoTerminado;
+    }
+
+    public void intentarLetra(char letra) {
+        if (juegoTerminado) {
+            return;
+        }
+        if (letraYaIntentada(letra)) {
+            return;
+        }
+        if (palabra.contains(String.valueOf(letra))) {
+            destaparLetra(letra);
+            letrasIntentadas[intentos] = letra;
+            intentos++;
+
+
+        } else {
+            letrasIntentadas[intentos] = letra;
+            intentosIncorrectos++;
+            intentos++;
+            pintarLinea();
+        }
+        if (palabraOculta.equals(palabra)) {
+            juegoTerminado = true;
+        }
+    }
+
+    private boolean letraYaIntentada(char letra) {
+        for (char letraIntentada : letrasIntentadas) {
+            if (letraIntentada == letra) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void destaparLetra(char letra) {
+        char[] palabraOcultaArray = palabraOculta.toCharArray();
+        char[] palabraArray = palabra.toCharArray();
+        for (int i = 0; i < palabraArray.length; i++) {
+            if (palabraArray[i] == letra) {
+                palabraOcultaArray[i] = letra;
+            }
+        }
+        palabraOculta = String.valueOf(palabraOcultaArray);
+    }
+
+    public static String getLetrasIntentadas() {
+        return String.valueOf(letrasIntentadas);
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public static void pintarLinea() {
+        switch (intentosIncorrectos) {
+            case 1:
+                dibujo = "______\n" +
+                        "|    |\n" +
+                        "|    O\n" +
+                        "|    \n" +
+                        "|______\n";
+                break;
+            case 2:
+                dibujo = "______\n" +
+                        "|    |\n" +
+                        "|    O\n" +
+                        "|    |\n" +
+                        "|______\n";
+                break;
+            case 3:
+                dibujo = "______\n" +
+                        "|    |\n" +
+                        "|    O\n" +
+                        "|   /|\n" +
+                        "|______\n";
+                break;
+            case 4:
+                dibujo = "______\n" +
+                        "|    |\n" +
+                        "|    O\n" +
+                        "|   /|\\\n" +
+                        "|______\n";
+                break;
+            case 5:
+                dibujo = "______\n" +
+                        "|    |\n" +
+                        "|    O\n" +
+                        "|   /|\\\n" +
+                        "|   /   \n" +
+                        "|______\n";
+                break;
+            case 6:
+                dibujo = "______\n" +
+                        "|    |\n" +
+                        "|    O\n" +
+                        "|   /|\\\n" +
+                        "|   / \\\n" +
+                        "|______\n";
+                juegoTerminado = true;
+                break;
+
+            case 0:
+                dibujo = "______\n" +
+                        "|    |\n" +
+                        "|    \n" +
+                        "|    \n" +
+                        "|______\n";
+                break;
+        }
+    }
+
+    public static String MostrarDibujo() {
+        return dibujo;
+
+    }
+
 
 
 
---
+++ Programacion/Ejercicios_UD7/EX_7_1/src/ahoracdo_main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class ahoracdo_main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] palabras = {
+                "casa", "perro", "libro", "árbol", "mesa",
+                "silla", "sol", "luna", "mar", "montaña",
+                "ciudad", "coche", "flor", "reloj", "café",
+                "música", "tiempo", "color", "papel", "agua",
+                "fuego", "tierra", "aire", "luz", "noche",
+                "día", "pan", "amor", "vida", "mano",
+                "puerta", "ventana", "jardín", "cielo", "río",
+                "piedra", "bosque", "playa", "estrella", "pájaro",
+                "camino", "lluvia", "viento", "nube", "corazón",
+                "familia", "amigo", "trabajo", "sueño", "paz"
+        };
+
+        Ahorcado ahorcado = new Ahorcado(palabras[(int) (Math.random() * palabras.length)]);
+
+        while (!ahorcado.isJuegoTerminado()) {
+            System.out.println(ahorcado.getPalabraOculta());
+            System.out.println("Letras intentadas: " + Ahorcado.getLetrasIntentadas());
+            System.out.println("Introduce una letra: ");
+            Ahorcado.pintarLinea();
+            System.out.println(Ahorcado.MostrarDibujo());
+            char letra = sc.nextLine().charAt(0);
+            ahorcado.intentarLetra(letra);
+
+            if (Ahorcado.intentosIncorrectos == 6) {
+                System.out.println("Has perdido");
+                System.out.println("La palabra era: " + ahorcado.palabra);
+                break;
+            }
+            if (Ahorcado.intentosIncorrectos > 3) {
+                System.out.println("Cuidado, te quedan " + (6 - Ahorcado.intentos) + " intentos");
+                System.out.println("Escribe * para adivinar la palabra");
+                String respuesta = sc.nextLine();
+                if (respuesta.equals("*")) {
+                    System.out.println("Introduce la palabra: ");
+                    String palabra = sc.nextLine();
+                    if (palabra.equals(ahorcado.palabra)) {
+                        System.out.println("Has ganado");
+                        break;
+                    } else {
+                        System.out.println("Incorrecto, escriba otra letra");
+                    }
+                }
+            }
+        }
+    }
+}
