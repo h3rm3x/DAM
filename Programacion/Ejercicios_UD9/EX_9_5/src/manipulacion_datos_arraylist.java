e61a7233a67c75b5c1961f01070c91dbbaf873fa
h3rm3x <alanrabinerson@gmail.com>
2025-03-20 20:21:48 +0100

    terminados los ejercicios 5 6 y 8

--- Programacion/Ejercicios_UD9/EX_9_5/src/manipulacion_datos_arraylist.java
+++ Programacion/Ejercicios_UD9/EX_9_5/src/manipulacion_datos_arraylist.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 public class manipulacion_datos_arraylist {
     public static void main(String[] args) {
         // crear arraylist de colores
@@ -32,9 +32,46 @@ public class manipulacion_datos_arraylist {
         colores.sort(String::compareTo);
         // copiar la arraylist en otra y desordenarla automaticamente
         ArrayList<String> copia_colores = colores;
-        System.out.println( copia_colores.stream().sorted());
+        Collections.shuffle(copia_colores);
+        System.out.println( copia_colores);
 
-        //
+        // invertir los elementos de la lista
+        Collections.reverse(colores);
+        System.out.println( colores);
+        // extraer una parte del ArrayList
+
+        copia_colores.subList(1,3);
+        System.out.println(copia_colores);
+        // comparar dos arraylists
+        System.out.println( colores.equals(copia_colores));
+        // unir dos arraylists
+        colores.addAll(copia_colores);
+
+        // intercambiar dos elementos de un Arraylist
+        Collections.swap(colores,0,1);
+
+        // clonar una arraylist en otra
+        copia_colores = new ArrayList<>(colores);
+
+        // vaciar una arraylist
+        colores.clear();
+
+        // comprobar si un arraylist esta vacio
+        System.out.println(copia_colores.isEmpty());
+
+        //recortar una arraylist a la longitud actual
+        colores.trimToSize();
+
+        // aumentar la capacidad de una arraylist
+        copia_colores.ensureCapacity(10);
+
+        // sustituir un elemento de la arraylist por otro
+        copia_colores.set(0, "Naranja");
+        // imprimir la arraylist por los indices
+        for (int i = 0; i < copia_colores.size(); i++) {
+            System.out.println(copia_colores.get(i));
+        }
+
 
     }
 }
