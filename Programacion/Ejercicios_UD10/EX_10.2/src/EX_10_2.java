ddc5f3467275a3daa7de379fabb673c8bce55f8d
h3rm3x <alanrabinerson@gmail.com>
2025-04-03 17:31:11 +0200

    ex 10.1 y 10.2

---
+++ Programacion/Ejercicios_UD10/EX_10.2/src/EX_10_2.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class EX_10_2 {
+    public static void main(String[] args) {
+        File dir = new File("C:\\Users\\alanr\\Downloads");
+        File[] files = dir.listFiles();
+        for (File f : files) {
+
+        }
+
+
+    }
+}
