1902aea50f098eb467b3648e83a1c27844930241
h3rm3x <alanrabinerson@gmail.com>
2025-04-04 16:58:12 +0200

    correccion 10.3 y terminado 10.6

--- Programacion/Ejercicios_UD10/EX_10.3/src/ex_10_3.java
+++ Programacion/Ejercicios_UD10/EX_10.3/src/ex_10_3.java
@@ -6,16 +6,27 @@ public class ex_10_3 {
             OutputStream out = new FileOutputStream("C:\\Users\\alanr\\Documents\\DAM\\Programacion\\Ejercicios_UD10\\ex_10_2_copia.java");
             byte[] datos = new byte[1024];
             int leidos = 0;
+            int iteraciones = 0;
+            int bytesleidos = 0;
             while ((leidos = in.read(datos)) != -1) {
                 out.write(datos, 0, leidos);
+                iteraciones++;
+                for (int i = 0; i < datos.length; i++) {
+                    if (datos[i]== 0) {
+                        bytesleidos= i;
+                    }
+
+                }
+                bytesleidos+=(iteraciones-1)*1024;
             }
             in.close();
             out.close();
+            File original = new File("C:\\Users\\alanr\\Documents\\DAM\\Programacion\\Ejercicios_UD10\\EX_10.2\\src\\EX_10_2.java");
+            File copia = new File("C:\\Users\\alanr\\Documents\\DAM\\Programacion\\Ejercicios_UD10\\ex_10_2_copia.java");
             System.out.println("El archivo se ha copiado correctamente");
-            System.out.println("Ruta del archivo copiado: " + new File("C:\\Users\\alanr\\Documents\\DAM\\Programacion\\Ejercicios_UD10\\ex_10_2_copia.java").getAbsolutePath());
+            System.out.println("longitud del archivo original: " + original.length()+" longitud del archivo copia: " + copia.length()+ " bytes copiados " + bytesleidos+" bytes");
         }catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
     }
 }
