77b51abc4583a380214a2c07f777d5ab853d5e2e
h3rm3x <alanrabinerson@gmail.com>
2025-04-09 19:45:20 +0200

    Ejemplo insertar/leer objetos y ex_10_8

---
+++ Programacion/Ejercicios_UD10/EX_10.8/src/Persona.java
@@ -0,0 +1,44 @@
+// Clase base Persona
+public class Persona {
+    private String nombre;
+    private String dni;
+    private int edat;
+
+    // Constructor
+    public Persona(String nombre, String dni, int edat) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edat = edat;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edat;
+    }
+
+    public void setEdat(int edad) {
+        this.edat = edad;
+    }
+
+    // Método para mostrar información
+    public String toString() {
+       return "Nom" + nombre + ", DNI: " + dni + ", Edat: " + edat;
+    }
+}
+
---
+++ Programacion/Ejercicios_UD10/ejemplo_objeto_persona/src/personas.java
@@ -0,0 +1,32 @@
+import java.io.Serializable;
+
+public class personas implements Serializable {
+    private String nombre;
+    private int edad;
+    private String dni;
+
+    public personas(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
