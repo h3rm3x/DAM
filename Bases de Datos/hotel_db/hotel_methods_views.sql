8acf0c17aacbf86765bd5ac20634721564910aa0
h3rm3x <alanrabinerson@gmail.com>
2025-04-14 17:32:31 +0200

    fine tuning

--- Bases de Datos/hotel_db/hotel_methods_views.sql
+++ Bases de Datos/hotel_db/hotel_methods_views.sql
@@ -122,5 +122,5 @@ WHERE reservation_id = var_reservation_id;
 
 -- reservation view
 CREATE VIEW reservation_view AS
-SELECT r.reservation_id, r.room_number, r.customer_id, r.check_in, r.check_out, r.number_of_guests, c.first_name, c.last_name, c.email, c.phone_number, c.address, r.price_per_night*(DATEDIFF(r.check_out,r.check_in)) +iterate_extras(r.reservation_id) AS subtotal
+SELECT r.reservation_id, r.room_number, r.customer_id, r.check_in, r.check_out, r.number_of_guests, c.first_name, c.last_name, c.email, c.phone_number, c.address, r.price_per_night*(DATEDIFF(r.check_out,r.check_in)) AS subtotal
 FROM reservation r
