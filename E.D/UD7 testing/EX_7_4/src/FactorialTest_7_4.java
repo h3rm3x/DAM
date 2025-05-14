1c4a7ca6b5d410d830f584b15ffafd351540fe3f
h3rm3x <alanrabinerson@gmail.com>
2025-05-14 17:56:16 +0200

    correccion 7.4 7.5 y ejercicio repaso

--- E.D/UD7 testing/EX_7_4/src/FactorialTest_7_4.java
+++ E.D/UD7 testing/EX_7_4/src/FactorialTest_7_4.java
@@ -1,27 +1,25 @@
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FactorialTest_7_4 {
 
-    @Test
-    void calculo() {
-        assertAll(
-                () -> assertEquals(1, Factorial.calculo(0)),
-                () -> assertEquals(1, Factorial.calculo(1)),
-                () -> assertEquals(2, Factorial.calculo(2)),
-                () -> assertEquals(6, Factorial.calculo(3)),
-                () -> assertEquals(24, Factorial.calculo(4)),
-                () -> assertEquals(120, Factorial.calculo(5)),
-                () -> assertEquals(720, Factorial.calculo(6)),
-                () -> assertEquals(5040, Factorial.calculo(7)),
-                () -> assertEquals(40320, Factorial.calculo(8)),
-                () -> assertEquals(362880, Factorial.calculo(9)),
-                () -> assertEquals(3628800, Factorial.calculo(10)),
-                //() -> assertEquals(2.6525285981219105863630848e+32, Factorial.calculo(30)),
-                // per damunt de 20 dona o error de calcul o numero massa gran
-                () -> assertThrows(ArithmeticException.class, () -> Factorial.calculo(20))
+    @ParameterizedTest
+    @CsvSource(value = {"4,24", "3,6", "5,120", "6,720", "7,5040", "8,40320", "9,362880", "10,3628800"})
+    void calculoTest(int a, int resultado) {
+        assertEquals(Factorial.calculo(a), resultado);
+    }
 
-        );
+    @ParameterizedTest
+    @ValueSource(ints = {-3, -10, -20})
+    void calculoNegatius(int num) {
+        assertThrows(IllegalArgumentException.class, () -> Factorial.calculo(num));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {9999,20})
+    void calculoOverflow(int num) {
+        assertThrows(ArithmeticException.class, () -> Factorial.calculo(num));
     }
 }
