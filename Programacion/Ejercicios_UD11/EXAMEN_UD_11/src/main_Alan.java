d89db34dca5739eebd8bf1e65f2e5fef7fc271bc
alan <arabinerson20992@iesjoanramis.org>
2025-05-29 18:24:34 +0200

    Examen ud_11

---
+++ Programacion/Ejercicios_UD11/EXAMEN_UD_11/src/main_Alan.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class main_Alan {
+    public static void main(String[] args) {
+        Examen_ud11_Alan programa = new Examen_ud11_Alan();
+        programa.panelPrincipal.setVisible(true);
+        programa.panelPrincipal.setSize(500, 500);
+        programa.panelPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
--- Programacion/Ejercicios_UD11/Mastermind/src/InterfazMastermind.java
+++ Programacion/Ejercicios_UD11/Mastermind/src/InterfazMastermind.java
@@ -9,28 +9,31 @@ import javax.swing.*;
 /**
  * @author alanr
  */
-public class InterfazMastermind extends JFrame {
+public class  InterfazMastermind extends JFrame {
     public InterfazMastermind() {
         initComponents();
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - Alan Rabinerson Aranda
+        scrollPane1 = new JScrollPane();
 
         //======== this ========
         var contentPane = getContentPane();
         contentPane.setLayout(new GridBagLayout());
-        ((GridBagLayout)contentPane.getLayout()).columnWidths = new int[] {0, 0, 0};
-        ((GridBagLayout)contentPane.getLayout()).rowHeights = new int[] {0, 0, 0, 0};
-        ((GridBagLayout)contentPane.getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
-        ((GridBagLayout)contentPane.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)contentPane.getLayout()).columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+        ((GridBagLayout)contentPane.getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0, 0};
+        ((GridBagLayout)contentPane.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)contentPane.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+        contentPane.add(scrollPane1, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 5), 0, 0));
         pack();
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - Alan Rabinerson Aranda
+    private JScrollPane scrollPane1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
