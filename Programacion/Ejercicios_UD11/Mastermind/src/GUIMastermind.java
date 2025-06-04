aa75af22a25c4f27cb708f3e7fec3cc631f8536d
h3rm3x <alanrabinerson@gmail.com>
2025-06-04 19:42:43 +0200

    logica del GUI mastermind empezada

--- Programacion/Ejercicios_UD11/Mastermind/src/GUIMastermind.java
+++ Programacion/Ejercicios_UD11/Mastermind/src/GUIMastermind.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.GroupLayout;
 /*
@@ -10,130 +11,171 @@ import javax.swing.GroupLayout;
 /**
  * @author alanr
  */
-public class GUIMastermind extends JFrame {
+public class GUIMastermind {
     public GUIMastermind() {
         initComponents();
     }
 
+    private void ComprobarTirada(ActionEvent e) {
+        String textotirada = fieldTiradas.getText().trim();
+        if (textotirada.length() != 4) {
+            JOptionPane.showMessageDialog(ContentPanel, "La tirada debe tener exactamente 4 caracteres.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        char[] combinacionIntentada = textotirada.toCharArray();
+        Tirada tirada = new Tirada(combinacionIntentada);
+        int[] resultado = Mastermind.partida.comprobar(tirada);
+
+
+
+        areaListaTiradas.append("Tirada: " + tirada + "Resultado: [" + resultado[0] + ", " + resultado[1] + ", " + resultado[2] +"]\n");
+        Mastermind.partida.getListaTiradas().add(tirada);
+        fieldTiradas.setText("");
+        if (Mastermind.partida.getEstadoFinal()) {
+            JOptionPane.showMessageDialog(ContentPanel, "¡Felicidades! Has adivinado la combinación secreta.", "Fin del juego", JOptionPane.INFORMATION_MESSAGE);
+            fieldTiradas.setEnabled(false);
+            btnComprobartirada.setEnabled(false);
+        } else if (Mastermind.partida.getListaTiradas().size() >= 16) {
+            JOptionPane.showMessageDialog(ContentPanel, "Has alcanzado el número máximo de tiradas. Fin del juego.", "Fin del juego", JOptionPane.INFORMATION_MESSAGE);
+            fieldTiradas.setEnabled(false);
+            btnComprobartirada.setEnabled(false);
+        }
+    }
+
+
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Alan Rabinerson Aranda
+        ContentPanel = new JFrame();
         menuBar1 = new JMenuBar();
-        menu1 = new JMenu();
-        menuItem1 = new JMenuItem();
-        menuItem2 = new JMenuItem();
-        label1 = new JLabel();
-        textField1 = new JTextField();
-        label2 = new JLabel();
-        button1 = new JButton();
-        scrollPane1 = new JScrollPane();
-        textArea1 = new JTextArea();
-        label3 = new JLabel();
+        menuPartidas = new JMenu();
+        itemNuevaPartida = new JMenuItem();
+        itemVerPartidas = new JMenuItem();
+        panelPartida = new JPanel();
+        lblTitulo = new JLabel();
+        fieldTiradas = new JTextField();
+        lblTirada = new JLabel();
+        btnComprobartirada = new JButton();
+        scrollListaTiradas = new JScrollPane();
+        areaListaTiradas = new JTextArea();
+        lblListaTiradas = new JLabel();
 
-        //======== this ========
-        var contentPane = getContentPane();
-
-        //======== menuBar1 ========
+        //======== ContentPanel ========
         {
+            var ContentPanelContentPane = ContentPanel.getContentPane();
+            ContentPanelContentPane.setLayout(new CardLayout());
 
-            //======== menu1 ========
+            //======== menuBar1 ========
             {
-                menu1.setText("Partidas");
 
-                //---- menuItem1 ----
-                menuItem1.setText("Ver Partidas");
-                menu1.add(menuItem1);
+                //======== menuPartidas ========
+                {
+                    menuPartidas.setText("Partidas");
 
-                //---- menuItem2 ----
-                menuItem2.setText("Guardar Partidas");
-                menu1.add(menuItem2);
+                    //---- itemNuevaPartida ----
+                    itemNuevaPartida.setText("Nueva Partida");
+                    menuPartidas.add(itemNuevaPartida);
+
+                    //---- itemVerPartidas ----
+                    itemVerPartidas.setText("Ver Partidas");
+                    menuPartidas.add(itemVerPartidas);
+                }
+                menuBar1.add(menuPartidas);
             }
-            menuBar1.add(menu1);
-        }
-        setJMenuBar(menuBar1);
+            ContentPanel.setJMenuBar(menuBar1);
 
-        //---- label1 ----
-        label1.setText("MASTERMIND");
-        label1.setFont(new Font("Inter", Font.BOLD, 20));
+            //======== panelPartida ========
+            {
+                panelPartida.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new
+                javax.swing.border.EmptyBorder(0,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax
+                .swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java
+                .awt.Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt
+                .Color.red),panelPartida. getBorder()));panelPartida. addPropertyChangeListener(new java.beans.
+                PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062ord\u0065r".
+                equals(e.getPropertyName()))throw new RuntimeException();}});
 
-        //---- label2 ----
-        label2.setText("Nueva Tirada");
-        label2.setFont(label2.getFont().deriveFont(label2.getFont().getStyle() | Font.BOLD));
+                //---- lblTitulo ----
+                lblTitulo.setText("MASTERMIND");
+                lblTitulo.setFont(new Font("Inter", Font.BOLD, 20));
 
-        //---- button1 ----
-        button1.setText("Comprobar");
+                //---- lblTirada ----
+                lblTirada.setText("Nueva Tirada");
+                lblTirada.setFont(new Font("Inter", Font.BOLD, 14));
+                lblTirada.setLabelFor(fieldTiradas);
 
-        //======== scrollPane1 ========
-        {
+                //---- btnComprobartirada ----
+                btnComprobartirada.setText("Comprobar");
+                btnComprobartirada.addActionListener(e -> ComprobarTirada(e));
 
-            //---- textArea1 ----
-            textArea1.setLineWrap(true);
-            scrollPane1.setViewportView(textArea1);
-        }
+                //======== scrollListaTiradas ========
+                {
+                    scrollListaTiradas.setViewportView(areaListaTiradas);
+                }
 
-        //---- label3 ----
-        label3.setText("Lista Tiradas");
-        label3.setFont(new Font("Inter", Font.BOLD, 13));
+                //---- lblListaTiradas ----
+                lblListaTiradas.setText("Lista Tiradas ");
+                lblListaTiradas.setFont(new Font("Inter", Font.BOLD, 14));
 
-        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
-        contentPane.setLayout(contentPaneLayout);
-        contentPaneLayout.setHorizontalGroup(
-            contentPaneLayout.createParallelGroup()
-                .addGroup(contentPaneLayout.createSequentialGroup()
-                    .addGap(195, 195, 195)
-                    .addComponent(label1)
-                    .addContainerGap(196, Short.MAX_VALUE))
-                .addGroup(contentPaneLayout.createSequentialGroup()
-                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-                        .addGroup(contentPaneLayout.createSequentialGroup()
-                            .addContainerGap()
-                            .addComponent(button1, GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE))
-                        .addGroup(GroupLayout.Alignment.LEADING, contentPaneLayout.createSequentialGroup()
-                            .addGap(31, 31, 31)
-                            .addGroup(contentPaneLayout.createParallelGroup()
-                                .addComponent(label2)
-                                .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))))
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 141, Short.MAX_VALUE)
-                    .addGroup(contentPaneLayout.createParallelGroup()
-                        .addComponent(label3, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
-                        .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 186, GroupLayout.PREFERRED_SIZE))
-                    .addGap(32, 32, 32))
-        );
-        contentPaneLayout.setVerticalGroup(
-            contentPaneLayout.createParallelGroup()
-                .addGroup(contentPaneLayout.createSequentialGroup()
-                    .addContainerGap()
-                    .addComponent(label1)
-                    .addGap(62, 62, 62)
-                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(label2)
-                        .addComponent(label3))
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                    .addGroup(contentPaneLayout.createParallelGroup()
-                        .addGroup(contentPaneLayout.createSequentialGroup()
-                            .addComponent(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                GroupLayout panelPartidaLayout = new GroupLayout(panelPartida);
+                panelPartida.setLayout(panelPartidaLayout);
+                panelPartidaLayout.setHorizontalGroup(
+                    panelPartidaLayout.createParallelGroup()
+                        .addGroup(panelPartidaLayout.createSequentialGroup()
+                            .addGap(184, 184, 184)
+                            .addComponent(lblTitulo)
+                            .addContainerGap(207, Short.MAX_VALUE))
+                        .addGroup(panelPartidaLayout.createSequentialGroup()
+                            .addGap(28, 28, 28)
+                            .addGroup(panelPartidaLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                .addComponent(lblTirada)
+                                .addComponent(fieldTiradas, GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
+                                .addComponent(btnComprobartirada, GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 125, Short.MAX_VALUE)
+                            .addGroup(panelPartidaLayout.createParallelGroup()
+                                .addComponent(scrollListaTiradas, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblListaTiradas))
+                            .addGap(20, 20, 20))
+                );
+                panelPartidaLayout.setVerticalGroup(
+                    panelPartidaLayout.createParallelGroup()
+                        .addGroup(panelPartidaLayout.createSequentialGroup()
+                            .addComponent(lblTitulo)
+                            .addGap(34, 34, 34)
+                            .addGroup(panelPartidaLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(lblTirada)
+                                .addComponent(lblListaTiradas))
                             .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(button1))
-                        .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 227, GroupLayout.PREFERRED_SIZE))
-                    .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        pack();
-        setLocationRelativeTo(getOwner());
+                            .addGroup(panelPartidaLayout.createParallelGroup()
+                                .addGroup(panelPartidaLayout.createSequentialGroup()
+                                    .addComponent(fieldTiradas, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(btnComprobartirada))
+                                .addComponent(scrollListaTiradas, GroupLayout.PREFERRED_SIZE, 197, GroupLayout.PREFERRED_SIZE))
+                            .addGap(0, 72, Short.MAX_VALUE))
+                );
+            }
+            ContentPanelContentPane.add(panelPartida, "card7");
+            ContentPanel.pack();
+            ContentPanel.setLocationRelativeTo(ContentPanel.getOwner());
+        }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - Alan Rabinerson Aranda
+    private JFrame ContentPanel;
     private JMenuBar menuBar1;
-    private JMenu menu1;
-    private JMenuItem menuItem1;
-    private JMenuItem menuItem2;
-    private JLabel label1;
-    private JTextField textField1;
-    private JLabel label2;
-    private JButton button1;
-    private JScrollPane scrollPane1;
-    private JTextArea textArea1;
-    private JLabel label3;
+    private JMenu menuPartidas;
+    private JMenuItem itemNuevaPartida;
+    private JMenuItem itemVerPartidas;
+    private JPanel panelPartida;
+    private JLabel lblTitulo;
+    private JTextField fieldTiradas;
+    private JLabel lblTirada;
+    private JButton btnComprobartirada;
+    private JScrollPane scrollListaTiradas;
+    private JTextArea areaListaTiradas;
+    private JLabel lblListaTiradas;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
