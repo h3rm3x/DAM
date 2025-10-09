443458dfb4cbb98e0c435edde81d32df7ce75848
h3rm3x <alanrabinerson@gmail.com>
2025-10-09 17:20:07 +0200

    ENTORNO CLIENTE: ejemplos DOM acabado y empezada practica guiada cine

--- Segundo/Entorno Cliente/DOM/script.js
+++ Segundo/Entorno Cliente/DOM/script.js
@@ -1,35 +1,22 @@
-const parrafo = document.querySelector("body > div:nth-child(3) > p:nth-child(2)");
-//console.log(parrafo);
+let enlaces = document.getElementsByTagName("a");
+let enlace5 = enlaces[4];
+let miDiv = document.getElementById("miDiv");
+let parrafos = document.getElementsByTagName("p");
+let divP = document.getElementById("divP");
+let creaP = document.getElementById("creaP");
+let polar = divP.getElementsByTagName("p")[divP.getElementsByTagName("p").length - 1].getElementsByTagName("img")[0];
+divP.insertBefore(parrafos[2], parrafos[1]);
+let miP = document.getElementById("miP");
 
-const titulo = document.querySelector("body > h1");
-//console.log(titulo);
-
-const parrafos = document.querySelectorAll("p");
-//console.log(parrafos);
-
-parrafos.forEach(parrafo => {
-    //console.log(parrafo);
+creaP.addEventListener("click", () => {
+    if (polar.alt) {
+        let texto = document.createTextNode(polar.alt);
+        let elemento = document.createElement("div");
+        let miEnlace = document.createElement("a");
+        elemento.appendChild(miEnlace);
+        polar.parentElement.appendChild(elemento);
+        polar.parentElement.replaceChild(texto, polar);
+        miP.style.color = "blue";
+    }
+    
 });
-
-const todosErrores = document.querySelectorAll(".error");
-
-todosErrores.forEach(error => {
-    //console.log(error);
-});
-
-const tituloPagina = document.getElementById("titulo-pagina");
-//console.log(tituloPagina);
-
-const errores = document.getElementsByClassName("error");
-console.log(errores);
-/* errores.forEach(error => {
-    console.log(error);
-}); */ // No funciona porque no es un array
-
-const parrafos2 = document.getElementsByTagName("p");
-console.log(parrafos2);
-
-for (let i = 0; i < parrafos2.length; i++) {
-    const p = parrafos2[i];
-    console.log(p);
-}
