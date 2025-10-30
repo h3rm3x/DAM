5658fff92804ddf67a7892819e80b342264b3ce5
h3rm3x <alanrabinerson@gmail.com>
2025-10-30 17:26:27 +0100

    DAWEC: terminado ejercicio formularios y popup

--- Segundo/Entorno Cliente/POP-UP/script.js
+++ Segundo/Entorno Cliente/POP-UP/script.js
@@ -1,9 +1,12 @@
+const contenedorPopup = document.querySelector('.contenedor-popup');
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup');
+const cerrarPopup = document.querySelector('.cierra-popup');
 const form = document.getElementById('form');
 const nombreusuario = document.getElementById('nombreusuario');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-
 //funciones
 function mostarError(input, mensaje) {
     const formControl = input.parentElement;
@@ -63,6 +66,25 @@ function comprobarlongitud(arrayLongitud) {
 // event listener
 form.addEventListener('submit', (e) => {
     e.preventDefault();  
+
+    if (document.querySelectorAll('.form-control.error').length === 0) {
+        contenedorPopup.style.display = 'none';
+        
+        const mensajeExito = document.createElement('div');
+        mensajeExito.innerText = 'Formulario enviado correctamente: '+ nombreusuario.value+', '+email.value;
+        document.body.appendChild(mensajeExito);
+        document.body.style.fontSize = '20px';
+        mensajeExito.style.color = 'green';
+        mensajeExito.style.display = 'inline';
+        setTimeout(() => {
+            mensajeExito.style.display = 'none';
+        }, 10000);
+    }
+    form.reset();
+
+});
+
+form.addEventListener('input', () => {
     esObligatorio([nombreusuario, email, password, password2]);
     comprobarlongitud([
         {input: nombreusuario, min: 3, max: 15},
@@ -71,4 +93,20 @@ form.addEventListener('submit', (e) => {
     ]);
     validarEmail(email);
     comprobarPasswords(password, password2);
+});
+
+
+button.addEventListener('click', () => {
+    contenedorPopup.style.display = 'flex';
+});
+
+cerrarPopup.addEventListener('click', () => {
+    contenedorPopup.style.display = 'none';
+    form.reset();
+    
+});
+contenedorPopup.addEventListener('click', (event) => {
+    if (popup.contains(event.target) === false) {
+        contenedorPopup.style.display = 'none';
+    }
 });
