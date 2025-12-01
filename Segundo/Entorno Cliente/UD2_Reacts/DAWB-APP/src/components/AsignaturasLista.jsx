f4902ac143ae3322dc8cb6841598de153443b2ab
h3rm3x <alanrabinerson@gmail.com>
2025-12-01 19:46:05 +0100

    DWEC: implementado mensaje de carga y error

--- Segundo/Entorno Cliente/UD2_Reacts/DAWB-APP/src/components/AsignaturasLista.jsx
+++ Segundo/Entorno Cliente/UD2_Reacts/DAWB-APP/src/components/AsignaturasLista.jsx
@@ -1,26 +1,28 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState } from 'react'
 import './AsignaturasLista.css'
 import { useFetch } from '../hooks/useFetch.jsx'
 
 export default function AsignaturasLista() {
     // const [asignaturas, setAsignaturas] =  useState([])
-
-    const { datos } = useFetch(url)
     const [url, setUrl] = useState('http://localhost:3000/asignaturas')
-    const fetchAsignaturas = useCallback(async () => {
-      const response = await fetch(url);
-      const json = await response.json();
-      setAsignaturas(json);
-    }, [url]);
-    useEffect(() => {
-      fetchAsignaturas();
-    }, [fetchAsignaturas]) // el array es una rray de dependencias, en el que especificaremos cuando queremos que se ejecute el codigo
-    // console.log("Asignaturas: ", asignaturas)
+    const [datos, cargando, error] = useFetch(url)
+    
+    // const fetchAsignaturas = useCallback(async () => {
+    //   const response = await fetch(url);
+    //   const json = await response.json();
+    //   setAsignaturas(json);
+    // }, [url]);
+    // useEffect(() => {
+    //   fetchAsignaturas();
+    // }, [fetchAsignaturas]) // el array es una rray de dependencias, en el que especificaremos cuando queremos que se ejecute el codigo
+    // // console.log("Asignaturas: ", asignaturas)
   return (
     <div className='asignaturas-lista'>
       <h2>Listado de Asignaturas</h2>
+      {cargando && <div>Cargando Asignaturas...</div>}
+      {error && <div className="error">{error}</div>}
       <ul>
-        {asignaturas.map((asignatura) => (
+        {datos && datos.map((asignatura) => (
             <li key={asignatura.id}>
                 <h3>{asignatura.nombre}</h3>
                 <p>Horas totales: {asignatura["horas-totales"]}</p>
--- Segundo/Entorno Cliente/UD2_Reacts/DAWB-APP/src/hooks/useFetch.jsx
+++ Segundo/Entorno Cliente/UD2_Reacts/DAWB-APP/src/hooks/useFetch.jsx
@@ -3,14 +3,29 @@ import { useState, useEffect} from 'react'
 export const useFetch = (url) => {
 
     const [datos, setDatos] =  useState(null);
+    const [cargando, setCargando] = useState(false);
+    const [error, setError] = useState(null);
+    
     useEffect(() => {
         const fetchDatos = async () => {
-            const response = await fetch(url);
-            const json = await response.json();
-            setDatos(json);
+            setCargando(true);
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                const json = await response.json();
+                setDatos(json);
+                setError(null);
+            } catch (error) {
+                setCargando(false);
+                setError("no se pudieron obtener los datos");
+                console.log(error.message);
+            }
+            setCargando(false);
         };
         fetchDatos();
     }, [url]);
-    return datos;
+    return [datos, cargando, error];
 }
 
