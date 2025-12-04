bd83424f9434075086e7cf8a8a9539dca7b14f08
h3rm3x <alanrabinerson@gmail.com>
2025-12-04 20:08:44 +0100

    DWEC: memory app teminado

--- Segundo/Entorno Cliente/UD2_Reacts/memory-app/src/App.jsx
+++ Segundo/Entorno Cliente/UD2_Reacts/memory-app/src/App.jsx
@@ -1,50 +1,88 @@
+import { useEffect, useState } from 'react';
 import './App.css'
-import { useEffect, useState } from 'react'
+import Carta from './components/Carta';
 
 function App() {
 
-  const [cartas, setCartas] = useState([])
-  const [intentos, setIntentos] = useState(0)
-  const [eleccion1, setEleccion1] = useState(null)
-  const [eleccion2, setEleccion2] = useState(null)
-
-  const handleEleccion = (carta) => {
-    console.log(carta)
-    eleccion1 ? setEleccion2(carta) : setEleccion1(carta)
-  }
-
-  useEffect(() => {
-
-  }, [])
+  const [cartas, setCartas] = useState([]);
+  const [turnos, setTurnos] = useState(0);
+  const [eleccionUno, setEleccionUno] = useState(null);
+  const [eleccionDos, setEleccionDos] = useState(null);
+  const [deshabilitado, setDeshabilitado] = useState(false);
 
   const imagenesCartas = [
-    {src: "/img/jokic.png"},
-    {src: "/img/Haliburton.png"},
-    {src: "/img/lebron.png"},
-    {src: "/img/kd.png"},
-    {src: "/img/curry.png"},
-    {src: "/img/luka.png"},
+    {src: "/img/jokic.png", "encontrada": false},
+    {src: "/img/Haliburton.png", "encontrada": false},
+    {src: "/img/lebron.png", "encontrada": false},
+    {src: "/img/kd.png", "encontrada": false},
+    {src: "/img/curry.png", "encontrada": false},
+    {src: "/img/luka.png", "encontrada": false}
   ]
 
   const barajar = () => {
-    const cartasBajadas = [...imagenesCartas, ...imagenesCartas]
-      .sort(() => Math.random() - 0.5)
-      .map((carta) => ({...carta, id: Math.random()*10}))
-    setCartas(cartasBajadas)
+    const cartasBarajadas = [...imagenesCartas, ...imagenesCartas]
+      .sort(() => Math.random()-0.5)
+      .map((carta) => ({...carta, id:Math.random()}));
+    
+    setCartas(cartasBarajadas);
+    setTurnos(0);
+  };
+
+  const handleEleccion = (carta) => {
+    console.log(carta);
+    eleccionUno ? setEleccionDos(carta) : setEleccionUno(carta);
   }
+
+  const resetear = () => {
+    setEleccionUno(null);
+    setEleccionDos(null);
+    setTurnos(turnosPrevios => turnosPrevios + 1);
+    setDeshabilitado(false);
+  }
+
+  useEffect(() => {
+    if(eleccionUno && eleccionDos){
+      setDeshabilitado(true);
+      if(eleccionDos.src === eleccionUno.src){
+        setCartas(cartasPrevias => {
+          return cartasPrevias.map((carta) => {
+            if(carta.src === eleccionUno.src){
+              return {...carta, encontrada: true}
+            }else {
+              return carta;
+            }
+          });
+        });
+        resetear();
+      } else {
+        setTimeout(() => {
+          resetear();
+        },1000)
+      }
+    }
+  }, [eleccionUno, eleccionDos]);
 
 
   return (
     <div className='App'>
       <h1>Memory App</h1>
       <button onClick={barajar}>Nueva Partida</button>
-      <div className='grid-cartas'>
+
+      <div><p>Turnos: {turnos}</p></div>
+      <div className="grid-carta">
         {
           cartas.map((carta) => (
-            <Carta key={carta.id} carta={carta} />
+            <Carta 
+              carta={carta}
+              key={carta.id}
+              handleEleccion={handleEleccion}
+              volteada={carta===eleccionUno || carta===eleccionDos || carta.encontrada}
+              deshabilitado={deshabilitado}
+            />
           ))
-        }  
+        }
       </div>
+      
     </div>
   )
 }
--- Segundo/Entorno Cliente/UD2_Reacts/memory-app/src/components/Carta.jsx
+++ Segundo/Entorno Cliente/UD2_Reacts/memory-app/src/components/Carta.jsx
@@ -1,14 +1,19 @@
-import 'Carta.css'
-export default function Carta(carta, handleEleccion) {
-    const handleClick = () => {
-        handleEleccion(carta)
+import './Carta.css';
+
+export default function Carta({carta, handleEleccion, volteada, deshabilitado}) {
+
+  const handleClick = () => {
+    if(!deshabilitado){
+      handleEleccion(carta);
     }
+  }
+
   return (
-    <div className='carta' onClick={handleClick}>
-        <div>
-            <img className='delante' src={carta.src} />
-            <img className='detras' src="/img/reverso.png" />
-        </div>
+    <div className='carta'>
+      <div className={volteada ? "volteada" : ""}>
+        <img className="delante" src={carta.src} />
+        <img className="detras" src="/img/reverso.png" onClick={handleClick} />
+      </div>
     </div>
   )
 }
