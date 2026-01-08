19a83c3e4ea2f41a39d1696e31ebaeb3b5969d70
h3rm3x <alanrabinerson@gmail.com>
2026-01-08 17:24:13 +0100

    react router

---
+++ Segundo/Entorno Cliente/UD2_Reacts/joc-catala/src/pages/Inici.jsx
@@ -0,0 +1,9 @@
+
+export default function Inici() {
+  return (
+    <div>
+      <h2>Inici</h2>
+      <p>Benvingut a l'aplicació per aprendre català jugant!</p>
+    </div>
+  )
+}
---
+++ Segundo/Entorno Cliente/UD2_Reacts/joc-catala/src/pages/Preguntes.jsx
@@ -0,0 +1,9 @@
+
+export default function Preguntes() {
+  return (
+    <div>
+      <h2>Jocs de Preguntes</h2>
+      <p>Selecciona un joc de preguntes per posar a prova els teus coneixements de català.</p>
+    </div>
+  )
+}
