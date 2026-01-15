708e275b40aa513cf02fbe0b7dda0af7fcccde27
h3rm3x <alanrabinerson@gmail.com>
2026-01-15 19:16:03 +0100

    DIW: fase 1 actividad postit terminada

--- Segundo/DIW/UD4_JQUERY/activitat_postit/script.js
+++ Segundo/DIW/UD4_JQUERY/activitat_postit/script.js
@@ -1,13 +1,47 @@
-$("#crearPostit").click(function() {
-    const postit = $('<div class="postit" contenteditable="true">Nuevo Post-it</div>');
-    const contenedores = ['#contenedor1', '#contenedor2'];
-    const randomIndex = Math.floor(Math.random() * 2);
-    const colores = ['#d1e7dd', '#f8d7da'];
-    $(postit).css('background-color', colores[randomIndex]);
-    const zona = randomIndex === 0 ? 'top' : 'bottom';
-    $(postit).css(zona, Math.floor(Math.random() * 400) + 'px');
-    const lado = randomIndex === 1 ? 'left' : 'right';
-    $(postit).css(lado, Math.floor(Math.random() * 600) + 'px');
-    $("main").append(postit);
+$(document).ready(function () {
+  const contenedores = ["#contenedor1", "#contenedor2", "#contenedor3"];
+  const popupColor = $(".pop-up-colores");
 
+  $("#crearPostit").click(function () {
+    popupColor.addClass("active");
+  });
+
+  function crearPostit(color) {
+    const postit = $(
+      '<div class="postit"><h3 contenteditable="true">Nueva tarea</h3> <p contenteditable="true" class="descripcion">Descripción de la tarea</p></div>'
+    );
+    $(postit).css("background-color", color);
+    const randomZona = Math.floor(Math.random() * 2);
+    const zona = randomZona === 0 ? "top" : "bottom";
+    $(postit).css(zona, Math.floor(Math.random() * 400) + "px");
+    $(postit).css("left", Math.floor(Math.random() * 300) + "px");
+    $(postit).append(
+      '<button class="minimizar"><img src="./assets/minimizar.png" alt="Minimizar"></button>'
+    );
+    $(postit).append(
+      '<button class="maximizar"><img src="./assets/maximizar.png" alt="Maximizar"></button>'
+    );
+    $("main").append(postit);
+    $(postit).find(".descripcion").hide();
+    popupColor.removeClass("active");
+  }
+
+  $(".pop-up-colores .color").click(function () {
+    const colorSeleccionado = $(this).css("background-color");
+    crearPostit(colorSeleccionado);
+  });
+
+  $("main").on("click", ".minimizar", function () {
+    $(this).closest(".postit").removeClass("maximizado");
+    $(this).closest(".postit").find(".descripcion").hide();
+  });
+
+  $("main").on("click", ".maximizar", function () {
+    $(this).closest(".postit").addClass("maximizado");
+    $(this).closest(".postit").find(".descripcion").show();
+  });
+
+  $('#cerrarPopUp').click(function () {
+    popupColor.removeClass("active");
+  });
 });
