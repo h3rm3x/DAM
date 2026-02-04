b193e82aa892885ea91539271d85a14450ced05e
h3rm3x <alanrabinerson@gmail.com>
2026-02-04 23:19:38 +0100

    DWEC: firebase

--- Segundo/Entorno Cliente/UD2_Reacts/joc-catala/src/pages/Login.jsx
+++ Segundo/Entorno Cliente/UD2_Reacts/joc-catala/src/pages/Login.jsx
@@ -1,62 +1,57 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../auth/AuthContext'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../auth/AuthContext";
 
 export default function Login() {
+  const { login, register } = useAuth();
+  const navigate = useNavigate();
 
-    const {login, register} = useAuth();
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
+  async function handleLogin(e) {
+    e.preventDefault();
 
-    async function handleLogin(e){
-      e.preventDefault();
-
-      try{
-        await login (email, password);
-        navigate('/vocabulari');
-      }catch (err){
-        setError('Error al iniciar sessió: ' + err.message);
-      }
+    try {
+      await login(email, password);
+      navigate("/vocabulari");
+    } catch (err) {
+      setError("Error al iniciar sessió: " + err.message);
     }
+  }
 
-    async function handleRegister(e){
-      e.preventDefault();
+  async function handleRegister(e) {
+    e.preventDefault();
 
-      try{
-        await register (email, password);
-        navigate('/vocabulari');
-      }catch (err){
-        setError('Error al registrar-se: ' + err.message);
-      }
+    try {
+      await register(email, password);
+      navigate("/vocabulari");
+    } catch (err) {
+      setError("Error al registrar-se: " + err.message);
     }
-
-    //function handleLogin() {
-      //login();
-      //navigate('/vocabulari');
-    //}
-    function entrarComAlumnat() {
-      login("alumnat");
-      navigate('/vocabulari');
-    }
-    function entrarComAdmin() {
-      login("admin");
-      navigate('/vocabulari');
-    }
-    
+  }
 
   return (
     <div>
       <h2>Login</h2>
-      <form action="">
-        <input type="email" placeholder='email' value={email} onChange={e => setEmail(e.target.value)} />
-        <input type="password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)} />
-        {error && <p style={{color: 'red'}}>{error}</p>}
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <button onClick={handleLogin}>Iniciar sessió</button>
         <button onClick={handleRegister}>Registrar-se</button>
       </form>
     </div>
-  )
+  );
 }
