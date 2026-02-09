f6c3bc9a6b86521dc322c5c1e3cce85869f901d4
h3rm3x <alanrabinerson@gmail.com>
2026-02-09 20:27:18 +0100

    DIW: each()

---
+++ Segundo/DIW/UD4_JQUERY/clase5/script.js
@@ -0,0 +1,9 @@
+// 1. afegir eleements score amb text i botns de + i - 
+// 2. sumar i restar emprant deegació
+// 3. Calcular total emprant .each()
+
+let arr = ["A", "B", "C"];
+
+$.each(arr, function(index, value) {
+    console.log(index, value);
+});
